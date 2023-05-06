694c573e6cdf0d018e8f63b13878e077ad1744de
Anas2332 <amunir2332@gmail.com>
2023-05-06 19:05:10 +0500

    Get radius as input

---
+++ Printdiameter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+    using namespace std;
+     main()
+{
+    float radius;
+    cout<<"Enter the radius";
+    cin>>radius;
+    float diameter;
+    float cicumference;
+    float area;
+    diameter =radius*2;
+    cicumference =2*3.14*radius;
+    area=3.14*radius*radius;
+cout<<"\nThe diameter of circle is"<<diameter;
+cout<<"\nThe circumference of circle is"<<cicumference;
+cout<<"\nThe area of circle is" <<area;
+}
---
+++ Printsum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+    using namespace std;
+int main()
+{
+    int i;
+    i=29;
+    int j;
+    j=30;
+    int x;
+    x=i+j;
+    cout<<x;
+}
